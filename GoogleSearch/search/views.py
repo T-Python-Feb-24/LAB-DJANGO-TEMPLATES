06ea4de98f97c3993ecc6a3fd0a5c46316b65c2c
Abdullah <.alhumidi.Ab@gmail.com>
2024-02-28 19:44:05 +0300

    adding lab django templats

---
+++ GoogleSearch/search/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'search/home.html')
+
+def terms_of_service(request):
+    return render(request, 'search/terms.html')
